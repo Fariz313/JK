6c366cf125fa0c75bbcf08f25936823e467a7133
your_username <your_email_address@example.com>
2023-06-02 16:05:31 +0700

    kiee

--- register.php
+++ register.php
@@ -1,7 +1,6 @@
 <?php
 include 'login.php';
 
-$register = "INSERT INTO user VALUES ('$_POST[no_hp]', '$_POST[email]', '$_POST[password]', '$_POST[username]');";
 
 if (mysqli_query($conn, $register)) {
     echo "Registrasi Berhasil, silahkan login!";
